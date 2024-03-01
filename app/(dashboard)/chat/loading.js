33b15227eb5fca5d246b71624b78bd2131b4b199
lh20020727 <lh20020727@sjtu.edu.cn>
2024-03-01 00:23:01 -0800

    add loading animation

---
+++ app/(dashboard)/chat/loading.js
@@ -0,0 +1,9 @@
+function ProfileLoading(props) {
+  return (
+    <div className="flex items-center justify-center h-screen w-screen">
+      <span className="loading loading-spinner text-primary"></span>
+    </div>
+  );
+}
+
+export default ProfileLoading;
